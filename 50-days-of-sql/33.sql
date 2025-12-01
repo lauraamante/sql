770bd653e9a7f615f08fc2d5ac5637d061d65a2d
Laura Amante <lauraoliveira.amante@gmail.com>
2025-12-01 15:38:18 -0300

    Update 33.sql

--- 50-days-of-sql/33.sql
+++ 50-days-of-sql/33.sql
@@ -1,13 +1,16 @@
-
+--Calcula o preço médio, arredonda e dá um nome para a coluna
 SELECT 
     c.category_name,
     ROUND(AVG(p.unit_price), 2) AS average_unit_price
 FROM 
     categories c
+    
 JOIN 
     products p ON p.category_id = c.category_id
+    
 GROUP BY 
     c.category_name
-
+    
+-- Ordena pelo preço médio, do maior para o menor
 ORDER BY 
     average_unit_price DESC;
