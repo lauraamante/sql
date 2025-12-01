3a46d7e37016b7e671ba9784b43b3017cfd67d98
Laura Amante <lauraoliveira.amante@gmail.com>
2025-12-01 15:35:38 -0300

    Fix SQL query to include GROUP BY clause

--- 50-days-of-sql/33.sql
+++ 50-days-of-sql/33.sql
@@ -2,8 +2,9 @@
 SELECT 
     c.category_name,
     ROUND(AVG(p.unit_price), 2) AS average_unit_price
-
 FROM 
     categories c
 JOIN 
     products p ON p.category_id = c.category_id
+GROUP BY 
+    c.category_name
