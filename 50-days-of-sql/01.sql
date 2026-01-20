928fb4ff2ef722b33225617294f43a1698615290
Laura Amante <lauraoliveira.amante@gmail.com>
2026-01-20 13:33:36 -0300

    Query admissions for same day admission and discharge

--- 50-days-of-sql/01.sql
+++ 50-days-of-sql/01.sql
@@ -1,3 +1,4 @@
 -- Your task is to write a SQL query to show all the columns from the admissions table where the patient was admitted and discharged on the same day.
 SELECT * FROM admissions
 WHERE admission_date = discharge_date
+  
